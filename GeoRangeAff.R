671efd2d2debd06876cf722e6f072210166f3d4e
Shira Salingre <shira.salin@gmail.com>
2019-12-18 19:46:49 +0200

    updated

--- GeoRangeAff.R
+++ GeoRangeAff.R
@@ -15,8 +15,5 @@ class(polygon)
 
 # convex halls (polygon) + environmental data (raster)
 library(raster)
-summary_gra <- raster::extract(x = temp, y = polygon) %>% as.() %>% na.omit()
-gra_mean <- mean(summary_gra$BO2_temprange_ss)
-gra_med <- median(summary_gra)
-gra_mode <- mode(summary_gra)
-gra_quant25 <- quantile(summary_gra)
+summary_gra <- raster::extract(x = temp, y = polygon) %>% as.() %>% na.omit() %>% summary()
+summary_gra
