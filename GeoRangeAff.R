7e500248a849e5fdb0bb7a210506399efb7b42f2
Shira Salingre <shira.salin@gmail.com>
2019-12-18 19:21:28 +0200

    updated

--- GeoRangeAff.R
+++ GeoRangeAff.R
@@ -1,13 +1,22 @@
 library(tidyverse)
 
-coords <- read_csv("coords.csv")
-
+# GetEnv
 library(sdmpredictors)
-all_bio_layers <- list_layers(datasets = "Bio-ORACLE", marine = TRUE) %>%
-  dplyr::select(layer_code, name) # to view layers names and code
-temp_layers <- all_bio_layers %>% filter(stringr::str_detect(.$name, "temperature"))
-
 temp <- load_layers(c("BO2_temprange_ss"), datadir = "Bio ORACLE data")
-mean_temp <- raster::extract(temp[[1]], coords, buffer = 15000, fun = mean, df = TRUE)
 
-plot(mean_temp)
+# GeoRange
+library(dismo)
+
+coords <- read_csv("coords.csv")
+cov <- convHull(coords)
+polygon <- polygons(cov)
+
+class(polygon)
+
+# convex halls (polygon) + environmental data (raster)
+library(raster)
+summary_gra <- raster::extract(x = temp, y = polygon) %>% as.() %>% na.omit()
+gra_mean <- mean(summary_gra$BO2_temprange_ss)
+gra_med <- median(summary_gra)
+gra_mode <- mode(summary_gra)
+gra_quant25 <- quantile(summary_gra)
