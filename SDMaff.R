469f05d534b2c6c58e1dc183ca53c75e623b95d3
jbelmaker <jbelmaker@tauex.tau.ac.il>
2019-12-18 17:54:59 +0200

    New clean version 1.1

--- SDMaff.R
+++ SDMaff.R
@@ -1,37 +1,27 @@
-<<<<<<< HEAD
 ################ Funnctions to perfomr SDMs and calculate species level thermal affinity matrices 
 #########
 ###Major input: GetCoords + GetEnv
 # Coords - data frame of z/ y (long / lat). Output of GetCoords  
 # Env - stacked enviromental layer. Output of GetEnv 
-=======
-### Funnctions to perfomr SDMS and calculate species level thermal affinity matrices 
->>>>>>> 2532cc60b57ba0fea240a4dc19ec0a0d644a70e7
 
 
-<<<<<<< HEAD
 ########## Define pseudoabsences, uses the the novel three-step method as described in Iturbide et al. (2015) 
 N=10 ##N - number of pseudoAbsences realizations  
 prev = 0.5 #ratio of presences to pseudoAbsences
-=======
->>>>>>> 2532cc60b57ba0fea240a4dc19ec0a0d644a70e7
 
 # Define pseudoabsences 
-
-
-GetPseudo <- function(GetCoords, GetEnv) {
+GetPseudo <- function(Coords, Env, N, prev) {
   library(mopa)
   
+  bg.profile <- OCSVMprofiling(xy = Coords, varstack = Env)
+  bagr.radius <- backgroundRadius(xy = Coords, background = bg.profile$absence,  unit = "decimal degrees") 
+  pseudo_TS <- pseudoAbsences(xy = GetCoords, realizations=N, background = bagr.radius, exclusion.buffer = 0.01, prevalence = 0.5)
+  return(pseudo_TS) 
   
 }
 
 #preform SDM
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 2532cc60b57ba0fea240a4dc19ec0a0d644a70e7
 SDM <- function(GetCoords, GetEnv, GetPseudo) {
   library(dismo)
   return(maxent(p=GetCoords, x=GetEnv, a=GetPseudo))
@@ -50,30 +40,7 @@ response(bc)
   }
   
   
-<<<<<<< HEAD
-=======
-  
-  temp_C <- (temp_F - 32) * 5 / 9
-  return(temp_C)
-  
-  return(temp_C) 
-  
-}
 
-
-
-SDMaff <- function(GetCoords, GetEnv) {
-  
-  
-  
-  
-  temp_C <- (temp_F - 32) * 5 / 9
-  return(temp_C)
-  
-  return(temp_C) 
-  
-}
->>>>>>> 2532cc60b57ba0fea240a4dc19ec0a0d644a70e7
 
 
 
