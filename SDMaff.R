2532cc60b57ba0fea240a4dc19ec0a0d644a70e7
Itai Granot <itai811@gmail.comgit config --global --listgit config --global user.name Itai>
2019-12-18 17:31:54 +0200

    SDM function

--- SDMaff.R
+++ SDMaff.R
@@ -1,31 +1,26 @@
-### Funnctions to perfomr SDMs and calculate species level thermal affinity matrices 
+### Funnctions to perfomr SDMS and calculate species level thermal affinity matrices 
 
 input: GetCoords + GetEnv
 
-# Define pseudoabsences, uses the the novel three-step method as described in Iturbide et al. (2015) 
-N=10 ##N - number of pseudoAbsences realizations  
-prev = 0.5 #ratio of presences to pseudoAbsences
-# Coords - data frame of z/ y (long / lat) 
 
-GetPseudo <- function(Coords, Env, N, prev) {
+# Define pseudoabsences 
+
+
+GetPseudo <- function(GetCoords, GetEnv) {
   library(mopa)
   
-  bg.profile <- OCSVMprofiling(xy = Coords, varstack = Env)
-  bagr.radius <- backgroundRadius(xy = Coords, background = bg.profile$absence,  unit = "decimal degrees") 
-  pseudo_TS <- pseudoAbsences(xy = GetCoords, realizations=N, background = bagr.radius, exclusion.buffer = 0.01, prevalence = 0.5)
-  return(pseudo_TS) 
   
 }
-
-
 
 #preform SDM
 
 
+
 SDM <- function(GetCoords, GetEnv, GetPseudo) {
-  
-  
+  library(dismo)
+  return(maxent(p=GetCoords, x=GetEnv, a=GetPseudo))
 } 
+
 
 #Extract affinity 
 
@@ -51,7 +46,11 @@ SDM <- function(GetCoords, GetEnv, GetPseudo) {
 
 SDMaff <- function(GetCoords, GetEnv) {
   
-
+  
+  
+  
+  temp_C <- (temp_F - 32) * 5 / 9
+  return(temp_C)
   
   return(temp_C) 
   
