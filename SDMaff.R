1b8b9acd8c5793515a001a9e1d4bad02e597565c
jbelmaker <jbelmaker@tauex.tau.ac.il>
2019-12-18 16:18:24 +0200

    version 1.0

---
+++ SDMaff.R
@@ -0,0 +1,59 @@
+### Funnctions to perfomr SDMS and calculate species level thermal affinity matrices 
+
+input: GetCoords + GetEnv
+
+
+# Define pseudoabsences 
+
+
+GetPseudo <- function(GetCoords, GetEnv) {
+  library(mopa)
+  
+  
+}
+
+#preform SDM
+
+
+SDM <- function(GetCoords, GetEnv, GetPseudo) {
+  
+  
+} 
+
+#Extract affinity 
+
+
+
+  SDMaff<-{
+    
+    
+    return(temp_C)  
+    
+  }
+  
+  
+  
+  temp_C <- (temp_F - 32) * 5 / 9
+  return(temp_C)
+  
+  return(temp_C) 
+  
+}
+
+
+
+SDMaff <- function(GetCoords, GetEnv) {
+  
+  
+  
+  
+  temp_C <- (temp_F - 32) * 5 / 9
+  return(temp_C)
+  
+  return(temp_C) 
+  
+}
+
+
+
+
